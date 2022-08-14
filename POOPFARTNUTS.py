2fd01692e907b27aa45cba14aa9834df9e98471d
Nulpickle <105951464+Nulpickle@users.noreply.github.com>
2022-08-14 03:41:08 -0400

    Add files via upload

---
+++ POOPFARTNUTS.py
@@ -0,0 +1,104 @@
+import os
+import time
+import shutil
+import webbrowser
+import ctypes
+a = 1
+while a > 0:
+
+
+    webbrowser.open('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9ad76d8c-4346-4ac3-96c7-90edc032fd4d/df1z9l5-704c1f92-44da-47e0-a2f0-d424b580b2b3.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlhZDc2ZDhjLTQzNDYtNGFjMy05NmM3LTkwZWRjMDMyZmQ0ZFwvZGYxejlsNS03MDRjMWY5Mi00NGRhLTQ3ZTAtYTJmMC1kNDI0YjU4MGIyYjMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.yy3jWAJ4TZ3Re5nD8zFPPY9UOgPvnkf1bUVQ8mW5CWo')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://www.youtube.com/watch?v=BNc9aAeczdU&t=405s')
+    time.wait = 0.1
+    webbrowser.open('https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9ad76d8c-4346-4ac3-96c7-90edc032fd4d/df1z9l5-704c1f92-44da-47e0-a2f0-d424b580b2b3.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzlhZDc2ZDhjLTQzNDYtNGFjMy05NmM3LTkwZWRjMDMyZmQ0ZFwvZGYxejlsNS03MDRjMWY5Mi00NGRhLTQ3ZTAtYTJmMC1kNDI0YjU4MGIyYjMuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.yy3jWAJ4TZ3Re5nD8zFPPY9UOgPvnkf1bUVQ8mW5CWo')
+    MessageBox = ctypes.windll.user32.MessageBoxW
+    MessageBox(None, 'Clearly You Dont Own An Air Fryer', 'POOP FART NUTS', 0)
